fd7f376845e2e7e2916b597e76f0629564186045
T.DAMAN <98676229+damantadi@users.noreply.github.com>
2024-12-24 19:28:33 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-del-mern-backend-k188.onrender.com'
+export const url = 'https://food-del-mern-backend-79a8.onrender.com'
