3ed645cd986ec30df176898349383aec7f4697c6
T.DAMAN <98676229+damantadi@users.noreply.github.com>
2024-12-25 06:17:41 +0530

    Update Header.jsx

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
     <div className='header'>
         <div className="header-contents">
             <h2>Order your favourite food here</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique dolorem, dicta itaque iure fugiat architecto.</p>
+            <p>Craving something delicious? Your favorite meals are just a tap away! From comforting classics to exciting new flavors, we bring the best dishes straight to your doorstep. Don't wait – satisfy your hunger and order now to enjoy a world of taste in just minutes!</p>
             <button>View Menu</button>
         </div>
     </div>
